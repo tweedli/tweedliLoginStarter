0c36c5170fdf16baa967b7436426f0ac42f77055
mccarthyist <TMcCarthy8@gmail.com>
2017-02-15 23:53:23 -0600

    removing comments

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-// Use cors so we can test our app from localhost, cors allows requests from localhost.
-var cors = require('cors');
-var port = port || 3000
-
 var app = express();
 
+var port = port || 3000
+
+var bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
+var cors = require('cors');
 app.use(cors());
 app.use(bodyParser.urlencoded({extended : true}))
 
@@ -20,10 +20,7 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 
-
-
-
-
+/**************** MONGOOSE/BCRYPT *****************/
 
 var bcrypt = require('bcryptjs')
 var mongoose = require('mongoose')
@@ -46,19 +43,18 @@ var tweedliUserSchema = new Schema({
 var TweedliUserCollection = mongoose.model('TweedliUserCollection', tweedliUserSchema)
 
 
-
 function authSignup(attrs){
 
 	return new Promise(function(resolve, reject){
 		bcrypt.hash(attrs.password, 0)
-		.then(function(hashPass){
-			console.log("hashPass" + hashPass)
-			
+		.then(function(hashPass){		
 			var user = new TweedliUserCollection({username: attrs.username, hashedPassword: hashPass})
 			user.save(function(err, info){
-				if(err) reject(err)
-				console.log(info)
-				resolve(info)
+				if(err){ 
+					reject(err)
+				} else {
+					resolve(info)
+				}
 			})
 		})
 		.catch(function(err){
@@ -71,99 +67,27 @@ function authLogin(attrs){
 	
 	return new Promise(function(resolve, reject){
 		TweedliUserCollection.findOne({username: attrs.username}, function(err, userInfo){
-			if(err) reject(err)
-			console.log(userInfo)
-			console.log("alkafa")
-			console.log(attrs.password + "\n" + userInfo.hashedPassword)
-			bcrypt.compare(attrs.password, userInfo.hashedPassword)
-			.then(function(x){
-				console.log(x)
-				if(x === true){
-					resolve(x)
-				} else {
-					reject(x)
-				}
-			})
-			.catch(function(err){
+			if(err) {
 				reject(err)
-			})
-		
+			} else {
+				bcrypt.compare(attrs.password, userInfo.hashedPassword)
+				.then(function(result){
+					if(result === true){
+						resolve(result)
+					} else {
+						reject(result)
+					}
+				})
+				.catch(function(err){
+					reject(err)
+				})
+			}
 		})
-
 	})
 }
 
 
-
-
-
-// exports.get = function(req, res){
-// 	let user = req.params.userName
-// 	console.log("user " + user)
-// 	TwitterUserCollection.findOne({user: user}) 
-// 	.then(data => {
-// 		if(data){
-// 		console.log("timeAdded " + data.timeAdded)
-// 			if(moment(data.timeAdded).add(10, 'days').isAfter(moment())){
-// 				res.status(200).send(data)
-// 			} else {
-// 				res.sendStatus(502)		
-// 			}
-// 		} else {
-// 			res.sendStatus(502)
-// 		}
-// 	})
-// 	.catch(err => {
-// 		console.log("???" + err)
-// 	})
-// }
-
-// exports.getTop = function(req, res){
-// 	let user = req.params.userName
-// 	console.log("user " + user)
-// 	TwitterUserCollection.findOne({user: user}) 
-// 	.then(data => {
-// 		console.log(data.topStatuses)
-// 		res.status(200).send(data.topStatuses)
-// 	})
-// 	.catch(err => {
-// 		console.log("???" + err)
-// 		res.sendStatus(502)
-// 	})
-// }
-
-// exports.insert = function(req, res){
-// 	let attrs = req.body
-// 	TwitterUserCollection.findOneAndUpdate({user: attrs.user}, attrs, {upsert: true})
-// 	.then(response => {
-// 		res.status(302)
-// 	})
-// 	.catch(x => {
-// 		res.sendStatus(502)
-// 	})
-// }
-
-// exports.insertTop = function(req, res){
-// 	let user  = req.body[0]
-// 	let attrs = req.body[1]
-// 	console.log(user)
-// 	console.log(attrs)
-// 	TwitterUserCollection.findOneAndUpdate({user: user}, attrs, {upsert: true})
-// 	.then(response => {
-// 		res.status(302)
-// 	})
-// 	.catch(x => {
-// 		res.sendStatus(502)
-// 	})
-// }
-
-
-
-
-
-
-
-
+/**************** ENDPOINTS *****************/
 
 app.post('/auth/signup', function(request, response){
 	authSignup(request.body)
